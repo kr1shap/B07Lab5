742f0f474a43494c3cce867bf7f1e0aa0aeeb66d
faizackhan <faiza.khanc@gmail.com>
2025-10-22 14:29:26 -0400

    feat: added Circle.java class

---
+++ Circle.java
@@ -0,0 +1,78 @@
+package lab4;
+
+public class Circle {
+	Point centre;
+	double radius; 
+	
+	// constructor
+	
+	public Circle(Point centre, double radius) {
+		 if (centre == null) {
+			 throw new IllegalArgumentException("Centre cannot be null");	 
+		 }
+		
+		 if (radius < 0) {
+			 throw new IllegalArgumentException("Radius cannot be negative");
+		 }
+		    
+		this.centre = centre;
+		this.radius = radius;
+	}
+	
+	// diameter 
+	
+	public double getDiameter() {
+		return 2*(this.radius);
+	}
+	
+	// circumference 
+	
+	public double getCircumference() {
+		
+		return 2 * radius * Math.PI ;
+	}
+	
+	// area 
+	
+	public double getArea() {
+
+		return Math.PI * radius * radius;
+	}
+	
+	// arc length 
+	
+	public double getArcLength(double angleInRadians) {
+		
+		// if angle too big, either simply or ask user try again
+		
+		return radius * angleInRadians;
+	}
+	
+	// overlap
+	
+	public boolean checkOverlap(Circle C) {
+		double x, y, d;
+		
+		x = this.centre.x - C.centre.x;
+		y = this.centre.y - C.centre.y;
+		
+		d = Math.sqrt((x*x) + (y*y));
+		
+		return (d <= (this.radius + C.radius)); 
+			
+	}
+	
+	// contains point
+	
+	public boolean containsPoint(Point p) {
+		double x,y,d;
+		
+		x = this.centre.x - p.x;
+		y = this.centre.y - p.y;
+		
+		d = Math.sqrt((x*x) + (y*y));
+		
+		return d <= radius;
+	}
+
+}
